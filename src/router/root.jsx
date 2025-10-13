2bb8274ad4116aa662599c203e96c48c9799bc40
jaesung <wkd6344@naver.com>
2025-10-14 02:54:48 +0900

    test

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -2,14 +2,20 @@ import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 
-const Loading = <div>Loading...</div>;
+const Loading = <div> <h1>로딩 중입니다</h1></div>;
 
 const Main = lazy(() => import("../pages/MainPage"));
+
+const Project = lazy(() => import ("../pages/ProjectPage"));
 
 const root = createBrowserRouter([
     {
         path:"",
         element: <Suspense fallback={Loading}><Main/></Suspense>
+    },
+    {
+        path: "project",
+        element: <Suspense fallback={Loading}><Project/></Suspense>
     }
 ]);
 
